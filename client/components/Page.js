635718a03b3a9b5fc580f52e46de900e347cb4d4
rockdev55 <steve264225@gmail.com>
2020-02-08 00:09:09 -0800

    Add global styles

--- client/components/Page.js
+++ client/components/Page.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Metadata from './Metadata';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const PageContainer = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 export default class Page extends Component {
   render() {
     return (
-      <div>
-        <Metadata />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Metadata />
+          <Header />
+          <PageContainer>{this.props.children}</PageContainer>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
